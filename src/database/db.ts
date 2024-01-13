a83a000862bd1aacbd795575dc7c44b8fc39e3b3
NitinN77 <nitinnarayanan77@gmail.com>
2024-01-13 17:19:42 +0530

    fix too many db clients issue during local dev

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,10 +1,23 @@
 import "dotenv/config"
-import { drizzle } from "drizzle-orm/postgres-js"
+import { PostgresJsDatabase, drizzle } from "drizzle-orm/postgres-js"
 import postgres from "postgres"
 
 const DB_URI = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
 
 export const migrationClient = postgres(DB_URI, { max: 1 })
 
-const queryClient = postgres(DB_URI)
-export const db = drizzle(queryClient)
+declare global {
+  var db: PostgresJsDatabase | undefined
+}
+
+let db: PostgresJsDatabase
+
+if (process.env.NODE_ENV === "production") {
+  db = drizzle(postgres(DB_URI))
+} else {
+  if (!global.db) global.db = drizzle(postgres(DB_URI))
+
+  db = global.db
+}
+
+export { db }
