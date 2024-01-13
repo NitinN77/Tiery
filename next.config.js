b29fe45fc880104bed82a4b5549f1505fd72c975
NitinN77 <nitinnarayanan77@gmail.com>
2024-01-13 20:56:14 +0530

    add s3 bucket hostname to next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
                 protocol: 'http',
                 hostname: '127.0.0.1',
                 port: '9002'
+            },
+            {
+                protocol: 'https',
+                hostname: 's3.ap-south-1.amazonaws.com',
+                port: ''
             }
         ]
     }
